53e86c1c58506b391ce88c1a788c8e50d0545555
zeen <157370421+kzeen@users.noreply.github.com>
2025-02-27 16:27:34 +0200

    Complete activity lifecycle logging

--- app/src/main/java/com/example/mobileweek5/MainActivity.java
+++ app/src/main/java/com/example/mobileweek5/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mobileweek5;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "ACTIVITY_LIFECYCLE"; // just a constant for accessing logs
+    // The tag is used to add to the logs so we can filter them in the log later
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.d(TAG, "onCreate"); // We can check the logs on logcat
+    }
+
+    @Override // Adding override tag helps us know if we are correctly overriding the default method - in this case, we have to call the super
+    public void onStart() {
+        // We call the super because we have to import the default code of the function, and we add to it
+        // but we don't alter its original way of working
+        // This super call is required for all lifecycle methods
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        // Rotating the screen will actually destroy the activity and create it again
     }
 }
